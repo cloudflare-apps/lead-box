8897dc0c45aa4b0dad844b228766d6db390a564d
Adam Schwartz <adamschwartz@users.noreply.github.com>
2015-07-28 02:36:20 -0500

    Implement form submissions powered by Formspree @zackbloom

--- eager-lead-box.js
+++ eager-lead-box.js
@@ -3,7 +3,7 @@
     return
   }
 
-  var options, style, el, show, hide, checkScroll, isPreview;
+  var options, style, el, form, show, hide, checkScroll, isPreview;
 
   options = INSTALL_OPTIONS;
 
@@ -42,6 +42,65 @@
   el.querySelector('.eager-lead-box-header').appendChild(document.createTextNode(options.headerText));
   el.querySelector('.eager-lead-box-body').appendChild(document.createTextNode(options.bodyText));
   el.querySelector('.eager-lead-box-button').appendChild(document.createTextNode(options.buttonText || '&nbsp;'));
+  if (options.email) {
+    form = el.querySelector('.eager-lead-box-form');
+    form.action = '//formspree.io/' + options.email;
+    form.addEventListener('submit', function(event){
+      event.preventDefault();
+
+      var header, body, button, url, xhr, callback, params;
+
+      header = el.querySelector('.eager-lead-box-header')
+      body = el.querySelector('.eager-lead-box-body')
+      button = el.querySelector('button[type="submit"]');
+      url = form.action;
+      xhr = new XMLHttpRequest();
+
+      if (isPreview) {
+        form.parentNode.removeChild(form);
+        header.innerHTML = options.successText;
+        body.innerHTML = '(Form submissions are simulated during the Eager preview.)'
+        return;
+      }
+
+      callback = function(xhr) {
+        var jsonResponse = {};
+
+        button.removeAttribute('disabled');
+
+        if (xhr && xhr.target && xhr.target.status === 200) {
+          form.parentNode.removeChild(form);
+          if (xhr.target.response) {
+            try {
+              jsonResponse = JSON.parse(xhr.target.response);
+            } catch (err) {}
+          }
+          if (jsonResponse && jsonResponse.success === 'confirmation email sent') {
+            body.innerHTML = 'Formspree has sent an email to ' + options.signupEmail + ' for verification.';
+          } else {
+            body.parentNode.removeChild(body);
+            header.innerHTML = options.successText;
+          }
+          setTimeout(hide, 3000);
+        } else {
+          body.innerHTML = 'Whoops, something didn’t work. Please try again.';
+        }
+      };
+
+      params = 'email=' + encodeURIComponent(el.querySelector('input[type="email"]').value);
+
+      if (!url) {
+        return;
+      }
+
+      button.setAttribute('disabled', 'disabled');
+      xhr.open('POST', url);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.onload = callback.bind(xhr);
+      xhr.send(params);
+    });
+  }
   document.body.appendChild(el);
 
   show = function() {
